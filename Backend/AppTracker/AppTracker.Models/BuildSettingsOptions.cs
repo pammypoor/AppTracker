7ce89d1ccf05e7270148c6d37b2412b1955f9ae7
pammypoor <36018468+pammypoor@users.noreply.github.com>
2022-06-14 18:06:57 -0700

    Configured Build Settings and Builder

---
+++ Backend/AppTracker/AppTracker.Models/BuildSettingsOptions.cs
@@ -0,0 +1,22 @@
+﻿namespace AppTracker.Models
+{
+    public class BuildSettingsOptions
+    {
+        public string Environment { get; set; } = String.Empty;
+        public string SqlConnectionString { get; set; } = String.Empty;
+        public string SendGridAPIKey { get; set; } = String.Empty;
+        public string JWTTokenKey { get; set; } = String.Empty;
+        public string JWTHeaderName { get; set; } = String.Empty;
+        public string AccessControlHeaderName { get; set; } = String.Empty;
+        public string RoleIdentityIdentifier1 { get; set; } = String.Empty;
+        public string RoleIdentityIdentifier2 { get; set; } = String.Empty;
+        public string User { get; set; } = String.Empty;
+        public string Admin { get; set; } = String.Empty;
+        public string JwtIssuer { get; set; } = String.Empty;
+        public string JwtAudience { get; set; } = String.Empty;
+        public string JwtHashAlgorithm { get; set; } = String.Empty;
+        public string GuestName { get; set; } = String.Empty;
+        public string GuestRole { get; set; } = String.Empty;
+        public string GuestHash { get; set; } = String.Empty;
+    }
+}
--- Backend/AppTracker/AppTracker.WebAPI/Program.cs
+++ Backend/AppTracker/AppTracker.WebAPI/Program.cs
@@ -1,11 +1,30 @@
+using AppTracker.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add Configuration File to DI
+builder.Services.Configure<BuildSettingsOptions>(
+    builder.Configuration.GetSection(nameof(BuildSettingsOptions)));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      builder =>
+                      {
+                          builder.WithOrigins(
+                                                "http://localhost:3000",
+                                                "https://localhost:3000")
+                                              .AllowAnyHeader()
+                                              .AllowAnyMethod()
+                                              .AllowCredentials();
+                      });
+});
 
 var app = builder.Build();
 
@@ -18,7 +37,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseCors(MyAllowSpecificOrigins);
+
+//app.UseTokenAuthentication();
 
 app.MapControllers();
 
